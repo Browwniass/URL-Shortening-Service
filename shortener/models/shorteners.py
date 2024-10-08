069767cfeeeff1e94543dcc70198e430b9f083cb
Browwniass <95659478+Browwniass@users.noreply.github.com>
2024-10-08 21:45:18 +0300

    feat: add indexation

--- shortener/models/shorteners.py
+++ shortener/models/shorteners.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class URLData(models.Model):
     url = models.URLField('URL', unique=True)
-    short_url = models.CharField(unique=True, blank=True)
+    short_url = models.CharField(unique=True, blank=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
