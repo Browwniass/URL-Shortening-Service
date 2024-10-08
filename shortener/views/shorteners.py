f3eff6dd64b9e0cb587bac1272fe06e1e07ddd67
Browwniass <95659478+Browwniass@users.noreply.github.com>
2024-10-08 21:33:59 +0300

    refactor: add extend_schema to views

--- shortener/views/shorteners.py
+++ shortener/views/shorteners.py
@@ -2,14 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponseRedirect
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from shortener.serializers.shorteners import URLDataSerializer
 from shortener.models.shorteners import URLData
 from shortener.encodings import generate_short_url
-from drf_spectacular.utils import extend_schema
 from shortener.serializers.shorteners import URLDataSerializer
 
 
+@extend_schema_view(
+    post=extend_schema(summary='Сокращение URL', tags=['Сокращатель_URL']),
+)
 class ShortenerView(APIView):
     serializer_class = URLDataSerializer
 
@@ -28,17 +31,19 @@ class ShortenerView(APIView):
             short_url = generate_short_url(url.id)
             url.short_url=short_url
             url.save()
-            
+
             return Response(result_url+short_url, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema_view(
+    get=extend_schema(summary='Перенаправка по сокращенному URL', tags=['Сокращатель_URL']),
+)
 class RedirectView(APIView):
     def get(self, request, subpath):
         url_data  = URLData.objects.filter(short_url=subpath).first()
-        if url_data  is not None:
-            attr = getattr(url_data , 'url')
-            redirect_url = attr
+        if url_data is not None:
+            redirect_url = getattr(url_data , 'url')
             return HttpResponseRedirect(redirect_url)
         return Response({'error': 'URL not found'}, status=status.HTTP_404_NOT_FOUND)
